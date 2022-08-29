71894cf0d93e36e050b03efaf38c9bdd414ef207
Metehan <metehan.bolat.ie@gmail.com>
2022-08-30 01:41:16 +0300

    Filter and Map

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,31 @@
 fun main() {
-    
+
+    val myNumList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    myNumList.filter { it == 4 }
+    myNumList.map { it * it }
+
+    val filterMapCombine = myNumList
+        .filter { it < 6 }
+        .map { it * it }
+
+    println(filterMapCombine)
+
+    val musician = listOf(
+        Musician("James", 60, "Guitar"),
+        Musician("Lars", 55, "Drum"),
+        Musician("Kirk", 50, "Guitar"),
+    )
+
+    val filteredMusicians = musician
+        .filter { it.age < 60 }
+        .map { it.instrument }
+
+    println(filteredMusicians)
 }
+
+data class Musician(
+    val name: String,
+    val age: Int,
+    val instrument: String
+)
